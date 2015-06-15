fc67759280ed2393730b6911d5bb89b6092b6cf2
Paul Wood III <paul.wood.iii@gmail.com>
2015-06-15 15:51:38 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+sudo yum update -y;
+sudo yum install gcc-c++ make -y;
+sudo yum install gcc -y;
+sudo yum install openssl-devel -y;
+sudo yum install git -y;
+sudo yum install glibc-static -y;
+sudo yum install ncurses-devel;
+git clone git://github.com/creationix/nvm.git ~/.nvm;
+source ~/.nvm/nvm.sh;
+echo "source ~/.nvm/nvm.sh" >> ~/.bash_profile;
+nvm install v0.10.28;
+nvm alias default v0.10.28;
+nvm which default;
+#vim ~/.ssh/authorized_keys;
+sudo yum update -y;
+sudo yum install docker -y;
+sudo service docker start;
+sudo docker pull paulwoodiii/baasbox;
