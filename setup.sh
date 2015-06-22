0cef7f4af527426fe4a63d0309b4eb964a643b78
Paul Wood III <paul.wood.iii@gmail.com>
2015-06-22 12:40:32 +0800

    rearranged the order and added some comments

--- setup.sh
+++ setup.sh
@@ -2,14 +2,16 @@ sudo yum update -y;
 sudo yum install gcc-c++ make -y;
 sudo yum install gcc -y;
 sudo yum install openssl-devel -y;
-sudo yum install git -y;
 sudo yum install glibc-static -y;
 sudo yum install ncurses-devel -y;
+#Install Git
+sudo yum install git -y;
+#Install Node.js
 git clone git://github.com/creationix/nvm.git ~/.nvm;
 source ~/.nvm/nvm.sh;
 echo "source ~/.nvm/nvm.sh" >> ~/.bash_profile;
-nvm install v0.10.28;
-nvm alias default v0.10.28;
+nvm install v0.10.35;
+nvm alias default v0.10.35;
 sudo yum install docker -y;
 sudo service docker start;
 sudo docker pull paulwoodiii/baasbox;
